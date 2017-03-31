bc8bc8dbce3c695800dbd844ad3883cdc6ec3da4
Chris Sheehan <stcts02@moravian.edu>
2017-03-31 12:49:45 -0400

    Refined skills so that timestamps are output as a human would say them rather than Alexa directly reading every character of the timestamp, also added intent that shuts down skill

--- caregiver.py
+++ caregiver.py
@@ -22,87 +22,117 @@ def print_response(webPage):
 def start_caregiver():
 	startMsg = 'Welcome to Caregiver.'
 	return question(startMsg)
+	#return statement(startMsg)
 
 @ask.intent("GetMedicineIntent")
 def medicineTime(patient):
 	medicineMsg = patient + 'took medicine at 7 15 AM.'
 	return question(medicineMsg).reprompt(questionMsg)
+	#return statement(medicineMsg)
 
 @ask.intent("GetWakeTimeIntent")
 def wakeTime(patient):
 	wakeTimeStatement = patient + 'woke up at 7 AM.'
 	return question(wakeTimeStatement).reprompt(questionMsg)
-	
+	#return statement(wakeTimeStatement)
+
+@ask.intent("GetMemoryGameIntent")
+def memGame(patient):
+	memGameDictArray = getData('memoryGame')
+	newMemGameData = memGameDictArray[0]
+	# data available: user(string), score(number), time(timestamp(ISO 8601 date))
+	if "time" in newMemGameData and "score" in newMemGameData:
+		time = convertTimeStamp(newMemGameData.get("time"))
+		score = newMemGameData.get("score")
+	memGameMsg = patient + " played memory game on " + str(time) + " and received a score of " + str(score)
+	return question(memGameMsg).reprompt(questionMsg)
+	#return statement(memGameMsg)
+
 @ask.intent("GetMemoryGameTimeIntent")
 def memGameTime(patient):
 	memGameTimeMsg = patient
-	memGameDictArray = getData('memoryGame') 
-	# user(string), score(number), time(timestamp(ISO 8601 date))
-	if "time" in memGameDictArray[0]:
-		memGameTimeMsg = memGameTimeMsg + ' last took memory Quiz at: ' + memGameDictArray[0].get("time")
+	memGameTimeDictArray = getData('memoryGame')
+	# data available: user(string), score(number), time(timestamp(ISO 8601 date))
+	if "time" in memGameTimeDictArray[0]:
+		time = convertTimeStamp(str(memGameTimeDictArray[0].get("time")))
+		memGameTimeMsg = memGameTimeMsg + ' last took memory Quiz at: ' + time
 	else:
 		memGameTimeMsg = 'There is no recorded time for the last memory quiz taken by ' + patient
 	return question(memGameTimeMsg).reprompt(questionMsg)
+	#return statement(memGameTimeMsg)
 
 @ask.intent("GetMemoryGameScoreIntent")
 def memGameScore(patient):
 	memGameScoreMsg = patient
-	memGameDictArray = getData('memoryGame')
-	# user(string), score(number), time(timestamp(ISO 8601 date))
-	if "score" in memGameDictArray[0]:
-		memGameScoreMsg = memGameScoreMsg + ' last score for the memory Quiz was: ' + memGameDictArray[0].get("score")
+	memGameScoreDictArray = getData('memoryGame')
+	# data available: user(string), score(number), time(timestamp(ISO 8601 date))
+	if "score" in memGameScoreDictArray[0]:
+		memGameScoreMsg = memGameScoreMsg + ' last score for the memory Quiz was: ' + str(memGameScoreDictArray[0].get("score"))
 	else:
 		memGameScoreMsg = 'There is no score for the last time ' + patient + ' took their memory quiz.'
 	return question(memGameScoreMsg).reprompt(questionMsg)
+	#return statement(memGameScoreMsg)
 
 @ask.intent("GetMemoryGameTimeAndScoreIntent")
 def memGameTimeAndScore(patient):
 	memGameFullMsg = patient
-	memGameDictArray = getData('memoryGame') 
-	# user(string), score(number), time(timestamp(ISO 8601 date))
-	if "time" in memGameDictArray[0] and "score" in memGameDictArray[0]:
-		memGameFullMsg = memGameFullMsg + ' last took memory Quiz at: ' + memGameDictArray[0].get("time") + " and their score was " + memGameDictArray[0].get("score")
+	memGameFullDictArray = getData('memoryGame')
+	# data available: user(string), score(number), time(timestamp(ISO 8601 date))
+	if "time" in memGameFullDictArray[0] and "score" in memGameFullDictArray[0]:
+		memGameFullMsg = memGameFullMsg + ' last took memory Quiz at: ' + str(memGameFullDictArray[0].get("time")) + " and their score was " + str(memGameFullDictArray[0].get("score"))
 	else:
 		memGameFullMsg = 'There is a missing time or score for the last memory quiz taken by ' + patient
 	return question(memGameFullMsg).reprompt(questionMsg)
+	#return statement(memGameFullMsg)
 
 @ask.intent("GetGPSIntent")
 def gps(patient):
 	gpsMsg = patient
 	gpsDictArray = getData('gps') 
-	# available: deviceID(string), time(timestamp(ISO 8601 date)), lat(number), lon(number) address(string)
+	# data available: deviceID(string), time(timestamp(ISO 8601 date)), lat(number), lon(number) address(string)
 	if "lat" in gpsDictArray[0] and "lon" in gpsDictArray[0] and "time" in gpsDictArray[0]:
 		lat = str(gpsDictArray[0].get("lat"))
 		lon = str(gpsDictArray[0].get("lon"))
-		time = str(gpsDictArray[0].get("time"))
-		gpsMsg = patient + 's last logged location was latitude: ' + lat + ' and longitude: ' + lon + " on " + time
+		time = convertTimeStamp(str(gpsDictArray[0].get("time")))
+		gpsMsg = patient + 's last logged location was latitude: ' + str(lat) + ' and longitude: ' + str(lon) + " on " + str(time)
 	else:
 		gpsMsg = 'latitude and longitude not available for ' + patient
 	return question(gpsMsg).reprompt(questionMsg)
+	#return statement(gpsMsg)
 	
 @ask.intent("GetWemoIntent")
 def wemo(patient):
 	wemoMsg = patient
 	wemoDictArray = getData('wemo') 
-	# available: date(string), time(string), status(boolean) off
+	# data available: date(string), time(string), status(boolean) off
 	# status: true-on, false-off
 	if "status" in wemoDictArray[0] and "time" in wemoDictArray[0] and "date" in wemoDictArray[0]:
 		if wemoDictArray[0].get("status") == True:
-			wemoMsg = wemoMsg + " lights have been on since " + wemoDictArray[0].get("time") + " on " + wemoDictArray[0].get("date")
+			wemoMsg = wemoMsg + " lights have been on since " + str(wemoDictArray[0].get("time")) + " on " + str(wemoDictArray[0].get("date"))
 		else:
-			wemoMsg = wemoMsg + " lights have been off since " + wemoDictArray[0].get("time") + " on " + wemoDictArray[0].get("date")
+			wemoMsg = wemoMsg + " lights have been off since " + str(wemoDictArray[0].get("time")) + " on " + str(wemoDictArray[0].get("date"))
 	return question(wemoMsg).reprompt(questionMsg)
+	#return statement(wemoMsg)
 
 @ask.intent("EndCaregiver")
 def endCaregiver():
 	endMsg = "Shutting down Caregiver, goodbye!"
 	return statement(endMsg)
 
-# returns array of dict objects
+# returns array of Dictionary objects
 def getData(endpoint_url):
 	full_url = url_to_use + '/' + endpoint_url
 	requestedAPIPage = requests.get(full_url)
 	return requestedAPIPage.json()
+
+def convertTimeStamp(time):
+#2017-03-30T19:28:38.136Z
+	timeArray = time.split("T")
+	date = timeArray[0]#.split("-")
+	time = timeArray[1].split(":")[0] + ":" + timeArray[1].split(":")[1]
+	newTime = date + " " + time
+	return newTime
+
   
 if __name__ == '__main__':
 	app.run(debug=True)  
