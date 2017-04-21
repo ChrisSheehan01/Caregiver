a8b8896a460b45cbcdf1efa36dd6c8fde51d5787
Chris Sheehan <stcts02@moravian.edu>
2017-04-21 16:21:32 -0400

    Updated journal intent and medicine intent to respond properly to journal data, can't fully test at the moment because the Pegasus URL no longer works, in addition, the intentSchema and sampleUtterences were updated to reflect changes with intents and should also be updated on Amazon Developer in order the Caregiver to work

--- caregiver.py
+++ caregiver.py
@@ -4,12 +4,15 @@ from flask_ask import Ask, statement, question, session
 import requests
 import time
 
+# TODO: PEGASUS URL IS DEAD, FIX
+# TODO: SEE IF wakeTime intent can be improved by data from Journal
+
 app = Flask(__name__)
 ask = Ask(app, "/")
 noEndpointMsg = 'no endpoint exists'
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 local_url = 'http://localhost:8080/api'
-pegasus_ngrok_url = 'https://8887eddd.ngrok.io/api'
+pegasus_ngrok_url = 'https://8887eddd.ngrok.io/api' # DEAD URL
 url_to_use = pegasus_ngrok_url
 questionMsg = 'Any other questions?'
 
@@ -17,24 +20,53 @@ def print_response(webPage):
     print "Request Url: ", webPage.url
     print "Response Status: ", webPage.status_code
     print "Response JSON: ", webPage.json()
-        
+
 @ask.launch
 def start_caregiver():
 	startMsg = 'Welcome to Caregiver.'
 	return question(startMsg)
-	#return statement(startMsg)
 
 @ask.intent("GetMedicineIntent")
 def medicineTime(patient):
-	medicineMsg = patient + 'took medicine at 7 15 AM.'
+	medicineMsg = patient
+	medicineJournDictArray = getData('journal')
+	if "medication" in medicineJournDictArray[0] and "date" in medicineJournDictArray[0]:
+		if medicineJournDictArray[0].get("medication") == True:
+			medicineMsg = "On " + medicineJournDictArray[0].get("datetime") + " " + medicineMsg + " recorded in their journal that they took their medicine today."
+		else:
+			medicineMsg = "On " + medicineJournDictArray[0].get("datetime") + " " + medicineMsg + " recorded in their journal that they did not take their medicine today."
+	else:
+		medicineMsg = "There is no record of the patient taking their medication today."
 	return question(medicineMsg).reprompt(questionMsg)
-	#return statement(medicineMsg)
+
+# apostrophe s used in question	-- "patient's" rather than "patient"
+@ask.intent("GetJournalIntent")
+def journal(patient):
+	journalMsg = patient
+	journalDictArray = getData('journal')
+	# data available: medication(boolean), message(string), datetime(timestamp), activities(String[])
+	if "medication" in journalDictArray[0] and "message" in journalDictArray[0] \
+			and "datetime" in journalDictArray[0] and "activities" in journalDictArray[0]:
+		if journalDictArray[0].get("medication") == True:
+			journalMsg = "On " + journalDictArray[0].get("datetime") \
+			+ " " + journalMsg \
+			+ " journal entry stated that they took their medicine today, " \
+			+ " their message was: " + journalDictArray[0].get("message") \
+			+ " and their activities were: " + journalDictArray[0].get("activities")
+		else:
+			journalMsg = "On " + journalDictArray[0].get("datetime") \
+				+ " " + journalMsg \
+				+ " journal entry stated that they did not take their medicine today, " \
+				+ " their message was: " + journalDictArray[0].get("message") \
+				+ " and their activities were: " + journalDictArray[0].get("activities")
+	else:
+		journalMsg = "There is insufficient data available in " + journalMsg + " most recent journal entry."
+	return question(journalMsg).reprompt(questionMsg)
 
 @ask.intent("GetWakeTimeIntent")
 def wakeTime(patient):
 	wakeTimeStatement = patient + 'woke up at 7 AM.'
 	return question(wakeTimeStatement).reprompt(questionMsg)
-	#return statement(wakeTimeStatement)
 
 @ask.intent("GetMemoryGameIntent")
 def memGame(patient):
@@ -46,7 +78,6 @@ def memGame(patient):
 		score = newMemGameData.get("score")
 	memGameMsg = patient + " played memory game on " + str(time) + " and received a score of " + str(score)
 	return question(memGameMsg).reprompt(questionMsg)
-	#return statement(memGameMsg)
 
 @ask.intent("GetMemoryGameTimeIntent")
 def memGameTime(patient):
@@ -59,7 +90,6 @@ def memGameTime(patient):
 	else:
 		memGameTimeMsg = 'There is no recorded time for the last memory quiz taken by ' + patient
 	return question(memGameTimeMsg).reprompt(questionMsg)
-	#return statement(memGameTimeMsg)
 
 @ask.intent("GetMemoryGameScoreIntent")
 def memGameScore(patient):
@@ -71,7 +101,6 @@ def memGameScore(patient):
 	else:
 		memGameScoreMsg = 'There is no score for the last time ' + patient + ' took their memory quiz.'
 	return question(memGameScoreMsg).reprompt(questionMsg)
-	#return statement(memGameScoreMsg)
 
 @ask.intent("GetMemoryGameTimeAndScoreIntent")
 def memGameTimeAndScore(patient):
@@ -83,7 +112,6 @@ def memGameTimeAndScore(patient):
 	else:
 		memGameFullMsg = 'There is a missing time or score for the last memory quiz taken by ' + patient
 	return question(memGameFullMsg).reprompt(questionMsg)
-	#return statement(memGameFullMsg)
 
 @ask.intent("GetGPSIntent")
 def gps(patient):
@@ -98,21 +126,20 @@ def gps(patient):
 	else:
 		gpsMsg = 'latitude and longitude not available for ' + patient
 	return question(gpsMsg).reprompt(questionMsg)
-	#return statement(gpsMsg)
-	
+
+# apostrophe s used in question	-- "patient's" rather than "patient"
 @ask.intent("GetWemoIntent")
 def wemo(patient):
 	wemoMsg = patient
 	wemoDictArray = getData('wemo') 
 	# data available: date(string), time(string), status(boolean) off
-	# status: true-on, false-off
+	# status: true = on, false = off
 	if "status" in wemoDictArray[0] and "time" in wemoDictArray[0] and "date" in wemoDictArray[0]:
 		if wemoDictArray[0].get("status") == True:
 			wemoMsg = wemoMsg + " lights have been on since " + str(wemoDictArray[0].get("time")) + " on " + str(wemoDictArray[0].get("date"))
 		else:
 			wemoMsg = wemoMsg + " lights have been off since " + str(wemoDictArray[0].get("time")) + " on " + str(wemoDictArray[0].get("date"))
 	return question(wemoMsg).reprompt(questionMsg)
-	#return statement(wemoMsg)
 
 @ask.intent("EndCaregiver")
 def endCaregiver():
@@ -125,14 +152,38 @@ def getData(endpoint_url):
 	requestedAPIPage = requests.get(full_url)
 	return requestedAPIPage.json()
 
-def convertTimeStamp(time):
-#2017-03-30T19:28:38.136Z
-	timeArray = time.split("T")
-	date = timeArray[0]#.split("-")
-	time = timeArray[1].split(":")[0] + ":" + timeArray[1].split(":")[1]
-	newTime = date + " " + time
-	return newTime
+def convertTimeStamp(timeStamp):
+    # FORMAT EXAMPLE: 2017-03-30T19:28:38.136Z
+    dateTimeArray = timeStamp.split("T")
 
-  
+    date = convertDate(dateTimeArray[0])
+    time = convert24HRTime(dateTimeArray[1].split(".")[0][:-3])
+
+    return (date + " " + time)
+
+def convert24HRTime(time):
+    hours = int(time[0:2])
+    if hours >= 12:
+        if hours != 12:
+            hours = hours - 12
+        ampm = " PM"
+    else:
+        if hours == 0:
+            hours = 12
+        ampm = " AM"
+    return (str(hours) + time[2:] + ampm)
+
+def convertDate(date):
+    dateArray = date.split("-")
+    year = dateArray[0]
+    month = dateArray[1]
+    day = dateArray[2]
+
+    months = ["January", "February", "March", "April",
+              "May", "June", "July", "August", "September",
+              "October", "November", "December"]
+
+    return months[int(month)-1] + " " + day + ", " + year
+
 if __name__ == '__main__':
 	app.run(debug=True)  
